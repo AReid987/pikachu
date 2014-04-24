78b1402532cb4b9cb9b70b05409f641767d03bd1
Andrey Repin <aarepin@gmail.com>
2014-04-24 21:23:32 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pikachu::Application.config.secret_token = '4825f6e6b5a0b8dad718900df6cc349964eddad24c7ca3c4049041e895de98f02418d25844548f1ebe6517f9f8d2200e25121175c358c504b4f36719b926cd1b'
