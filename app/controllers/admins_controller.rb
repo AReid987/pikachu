a40618b8745db414da7379fab9de8b1ed26a4baf
Andrey Repin <aarepin@gmail.com>
2014-05-13 12:05:44 +0300

    Admins were added

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,56 @@
+class AdminsController < ApplicationController
+	before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+
+ 	def index
+		@users = User.all
+		@admins = Admin.all
+	end
+
+	def new
+		@admin = Admin.new
+	end
+
+	def create
+	  @admin = Admin.new(params[:admin])
+	  if @admin.save
+	    redirect_to root_path, :notice => "#{admin.nickname} was succesfully created!"
+	  else
+	    render "new"
+	  end
+	end
+
+	def edit
+		@admin = Admin.find(params[:id])
+	end
+
+	def update
+		@admin = Admin.find(params[:id])
+    if @admin.update_attributes(params[:admin])
+      flash[:success] = "Admin was updated"
+      redirect_to root_path
+    else
+      render "edit"
+    end
+	end
+
+	private
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @admin = Admin.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def signed_in?
+    	!current_user.nil?
+  	end
+
+  	def current_user?(user)
+    	user == current_user
+  	end
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Pikachu::Application.routes.draw do
+  resources :admins
+  resources :users
+
   get "exit" => "sessions#destroy", :as => "exit"
   get "signin" => "sessions#new", :as => "signin"
   get "register" => "users#new", :as => "register"
   
   root :to => 'users#index'
 
-  resources :sessions
-  resources :users
 end
