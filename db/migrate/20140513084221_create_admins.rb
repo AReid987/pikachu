06c6476f2fdd22e1011f7e1221bed35d1ca822c7
Andrey Repin <aarepin@gmail.com>
2014-05-23 16:47:19 +0300

    admins were added to db

--- db/migrate/20140513084221_create_admins.rb
+++ db/migrate/20140513084221_create_admins.rb
@@ -1,7 +1,12 @@
 class CreateAdmins < ActiveRecord::Migration
   def change
     create_table :admins do |t|
-
+    	t.string :firstname
+    	t.string :lastname
+    	t.string :nickname
+    	t.string :email
+    	t.string :password_hash
+    	t.string :password_salt
       t.timestamps
     end
   end
