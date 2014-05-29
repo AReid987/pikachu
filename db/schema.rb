6e5e4b5be067df3dc4c4be12d6788a9244498bc0
Andrey Repin <aarepin@gmail.com>
2014-05-29 12:20:26 +0300

    roles for admins have been addded

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140513084221) do
+ActiveRecord::Schema.define(:version => 20140529082313) do
 
   create_table "admins", :force => true do |t|
     t.string   "firstname"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20140513084221) do
     t.string   "password_salt"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.string   "role"
   end
 
   create_table "users", :force => true do |t|
