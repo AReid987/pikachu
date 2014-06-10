660af6948e64dde4f26144e3b5395e5ecdb75239
Andrey Repin <aarepin@gmail.com>
2014-06-10 12:30:14 +0300

    route for sort, root was changed to users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Pikachu::Application.routes.draw do
   resources :admins
-  resources :users
+  
+  resources :users do
+  	collection do
+  		post :sort
+  	end
+  end
 
   resources :sessions
 	get "signout" => "sessions#destroy", :as => "signout"
 	get "signin" => "sessions#new", :as => "signin"
 	get "register" => "users#new", :as => "register"
   
-  root :to => 'users#new'
-  resources :sessions
+  root :to => "users#index"
 
 end
