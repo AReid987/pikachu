f3524aea97703a8c5173edecca2e3dee155bd56f
Andrey Repin <aarepin@gmail.com>
2014-06-13 19:38:43 +0300

    admin abilities moved to another file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,13 +5,4 @@ class Ability
     user ||= User.new
     can [:read, :update], User, :active => true, :user_id => user.id
   end
-
-  def initialize(admin)
-    admin ||= Admin.new
-    if admin.role == :super
-      can :manage, :all
-    else
-      can [:read, :update], [Admin, User]
-    end
-  end
 end
---
+++ app/models/admin_ability.rb
@@ -0,0 +1,11 @@
+class AdminAbility
+  include CanCan::Ability
+  
+  def initialize(admin)
+    if admin.role == "super"
+      can :manage, :all 
+    else
+      can [:read, :update], [Admin, User]
+  end
+end
+end
